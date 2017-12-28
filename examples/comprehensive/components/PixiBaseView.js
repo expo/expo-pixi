1d7d5f6169d1c2c257798dfe161fa27f58ee3345
Evan Bacon <baconbrix@gmail.com>
2017-12-27 21:14:17 -0600

    Adding comprehensive demo

---
+++ examples/comprehensive/components/PixiBaseView.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Expo from 'expo';
+
+import ExpoPixi from 'expo-pixi';
+
+import 'pixi.js';
+import 'url';
+import 'path';
+
+import TouchableView from './TouchableView';
+import { View, PixelRatio } from 'react-native';
+
+const scale = PixelRatio.get();
+function scaled({ x, y }) {
+  return { x: x * scale, y: y * scale };
+}
+export default ({ app }) => (
+  <TouchableView
+    id="pixi-view"
+    style={{ flex: 1 }}
+    onTouchesBegan={({ locationX: x, locationY: y }) => this.began && this.began(scaled({ x, y }))}
+    onTouchesMoved={({ locationX: x, locationY: y }) => this.moved && this.moved(scaled({ x, y }))}
+    onTouchesEnded={({ locationX: x, locationY: y }) => this.ended && this.ended(scaled({ x, y }))}
+    onTouchesCancelled={({ locationX: x, locationY: y }) =>
+      this.cancelled ? this.cancelled(scaled({ x, y })) : this.ended && this.ended(scaled({ x, y }))
+    }>
+    <View style={{ flex: 1 }}>
+      <Expo.GLView
+        style={{ flex: 1 }}
+        onContextCreate={async context => {
+          const events = (await app(context)) || {};
+          const { began, moved, ended, cancelled } = events;
+          this.began = began;
+          this.moved = moved;
+          this.ended = ended;
+          this.cancelled = cancelled;
+        }}
+      />
+    </View>
+  </TouchableView>
+);
