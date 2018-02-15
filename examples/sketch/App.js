6b45c8fd610fbc71b9d1ee0fe878ea1c6456c825
Evan Bacon <baconbrix@gmail.com>
2018-02-14 18:08:08 -0800

    0.2.1
    * Predefined lines in Sketch
    * pixi.js: 4.7.0
    * clear browser-polyfill
    * Removed circular reference in Sketch and FilteredImage
    * expo-asset-utils: 0.0.4-alpha.0

--- examples/sketch/App.js
+++ examples/sketch/App.js
@@ -1,14 +1,51 @@
 import Expo from 'expo';
 import * as ExpoPixi from 'expo-pixi';
 import React, { Component } from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, Platform, AppState, StyleSheet, View } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
+function uuidv4() {
+  //https://stackoverflow.com/a/2117523/4047926
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
 export default class App extends Component {
   state = {
     image: null,
     strokeColor: Math.random() * 0xffffff,
     strokeWidth: Math.random() * 30 + 10,
+    lines: [
+      {
+        points: [{ x: 300, y: 300 }, { x: 600, y: 300 }, { x: 450, y: 600 }, { x: 300, y: 300 }],
+        color: 0xff00ff,
+        alpha: 1,
+        width: 10,
+      },
+    ],
+    appState: AppState.currentState,
   };
+
+  handleAppStateChangeAsync = nextAppState => {
+    if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
+      if (isAndroid && this.sketch) {
+        this.setState({ appState: nextAppState, id: uuidv4(), lines: this.sketch.lines });
+        return;
+      }
+    }
+    this.setState({ appState: nextAppState });
+  };
+
+  componentDidMount() {
+    AppState.addEventListener('change', this.handleAppStateChangeAsync);
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this.handleAppStateChangeAsync);
+  }
 
   onChangeAsync = async ({ width, height }) => {
     const options = {
@@ -41,6 +78,7 @@ export default class App extends Component {
           strokeAlpha={1}
           onChange={this.onChangeAsync}
           onReady={this.onReady}
+          initialLines={this.state.lines}
         />
         <Image style={styles.image} source={this.state.image} />
       </View>
