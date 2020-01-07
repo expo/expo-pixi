ed234ab231ebad0fb28586a3f900824d6c6bdf0f
isocra <denish@isocra.com>
2020-01-07 10:51:59 +0000

    Add UNSAFE_ to componentWillMount

--- examples/signature/App.js
+++ examples/signature/App.js
@@ -1,72 +1,37 @@
-import Expo from 'expo';
 import * as ExpoPixi from 'expo-pixi';
-import React, { Component } from 'react';
-import { Image, Button, Platform, AppState, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button, StyleSheet, View } from 'react-native';
 
-const isAndroid = Platform.OS === 'android';
-function uuidv4() {
-  //https://stackoverflow.com/a/2117523/4047926
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = (Math.random() * 16) | 0,
-      v = c == 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-}
-
-export default class App extends Component {
-  state = {
-    image: null,
-    strokeColor: 0,
-    appState: AppState.currentState,
-  };
-
-  handleAppStateChangeAsync = nextAppState => {
-    if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
-      if (isAndroid && this.sketch) {
-        this.setState({ appState: nextAppState, id: uuidv4(), lines: this.sketch.lines });
-        return;
-      }
-    }
-    this.setState({ appState: nextAppState });
-  };
-
-  componentDidMount() {
-    AppState.addEventListener('change', this.handleAppStateChangeAsync);
-  }
-
-  componentWillUnmount() {
-    AppState.removeEventListener('change', this.handleAppStateChangeAsync);
-  }
+export default function App() {
+  let sketch = null;
 
   onReady = () => {
     console.log('ready!');
   };
 
-  render() {
-    return (
+  return (
+    <View style={styles.container}>
       <View style={styles.container}>
-        <View style={styles.container}>
-          <View style={styles.sketchContainer}>
-            <ExpoPixi.Signature
-              ref={ref => (this.sketch = ref)}
-              style={styles.sketch}
-              strokeColor={'blue'}
-              strokeAlpha={1}
-              onReady={this.onReady}
-            />
-          </View>
+        <View style={styles.sketchContainer}>
+          <ExpoPixi.Signature
+            ref={ref => (sketch = ref)}
+            style={styles.sketch}
+            strokeColor={'blue'}
+            strokeAlpha={1}
+            onReady={onReady}
+          />
         </View>
-        <Button
-          color={'blue'}
-          title="undo"
-          style={styles.button}
-          onPress={() => {
-            this.sketch.undo();
-          }}
-        />
       </View>
-    );
-  }
+      <Button
+        color={'blue'}
+        title="undo"
+        style={styles.button}
+        onPress={() => {
+          sketch.undo();
+        }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
